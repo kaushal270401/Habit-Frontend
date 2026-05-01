f2a85e571e6b667b85f9939b940389a3d2745227
kaushal <kaushal.k@idigicloudtech.com>
2026-05-02 01:55:26 +0530

    keycloak url changes

--- lib/keycloak.tsx
+++ lib/keycloak.tsx
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: "http://localhost:8080",
+  url: "https://habit-keycloak-1.onrender.com",
   realm: "habit-app",
   clientId: "habit-client"
 });
